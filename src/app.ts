bc628829904921202e6ff731674a9c3dc59687b6
MichaelDuo <michaeldong21@gmail.com>
2019-07-19 20:35:00 +0800

    add bodyParser

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
 import * as routers from './routers';
 
 const app = new Koa();
 
-app.use(routers.insecure.routes())
+app.use(bodyParser())
+	.use(routers.insecure.routes())
 	.use(routers.insecure.allowedMethods())
 	.use(routers.secure.routes())
 	.use(routers.secure.allowedMethods());
