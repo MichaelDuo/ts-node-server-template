e851e22cf6242d8a54c9e520cd5c3cff311a105f
MichaelDuo <michaeldong21@gmail.com>
2019-07-20 12:56:25 +0800

    add response time middleware

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import * as routers from './routers';
 import accessLog from './middlewares/accessLog';
+import responseTime from 'koa-response-time';
 
 const app = new Koa();
 
 app.use(accessLog())
+	.use(responseTime())
 	.use(bodyParser())
 	.use(routers.insecure.routes())
 	.use(routers.insecure.allowedMethods())
