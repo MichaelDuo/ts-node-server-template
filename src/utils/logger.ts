247270b07432692420b8b4fd0f19d4e6cc485551
MichaelDuo <michaeldong21@gmail.com>
2019-07-20 12:53:17 +0800

    format accessLog

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,7 +2,11 @@ import winston, {format} from 'winston';
 
 const logger = winston.createLogger({
 	level: 'info',
-	format: format.combine(format.timestamp(), format.json()),
+	format: format.combine(
+		format.timestamp(),
+		format.json(),
+		format.prettyPrint()
+	),
 	defaultMeta: {service: 'ts-node-server-template'},
 	transports: [new winston.transports.Console()],
 });
