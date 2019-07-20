a564d12de8779c7ab707dc8adaa8028731e26b18
MichaelDuo <michaeldong21@gmail.com>
2019-07-20 13:17:56 +0800

    catch error in main

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,17 @@ const port = 3000;
 async function main(): Promise<any> {
 	await bootstrap();
 	const server = http.createServer(app.callback());
+	server.keepAliveTimeout = 120 * 1000;
 	server.listen(port, (): void => {
 		logger.info(`App listening on port: ${port}`);
 	});
 }
 
-main();
+main().catch((error: Error): void => {
+	logger.error({
+		message: error.message,
+		stack: error.stack,
+	});
+	logger.info('Shutdown with error');
+	process.exit(1);
+});
